b94f0a987ae9d51e41fa0f5410194b88b95cf75c
AleksandraIliukhina <iliukhinaaleksandra@gmail.com>
2026-09-11 17:34:56 -0400

    Add files via upload
    A new version of power-digit-sum.cpp for homework 1 part 2.

--- power-digit-sum.cpp
+++ power-digit-sum.cpp
@@ -1,38 +1,82 @@
+// Aleksandra Iliukhina
+// Homework 1, part 2
+
 #include <iostream> // Connects input and output tools.
 #include <string> // Helps us to use string, since we need it in this code.
-#include <climits> // To check the maximum value.
+#include <climits> // Gives us information about limits of integer data types.
+#include <vector> // Allows us to create and work with vectors.
+#include <algorithm> // Provides functions for working with collections, for example, reverse().
+#include <cmath> // Provides mathematical functions, including log10() that we use for overflow check here.
 using namespace std; // So I don't have to write std:: every time. 
 
 
-// Calculates a to the power of b.
-long power(unsigned short a, unsigned short b){
-    long result = 1;
-    for (int i = 0; i < b; i++){
-        // If a is 0, the result will be 0.
-        if (a == 0){
-            return 0;
-        }
-        // Check if the result would become too large.
-        if (result > LONG_MAX / a){
-            cout << "Error: value is too large for long." << endl;
-            return -1;
-        }
+unsigned long long int power(unsigned int a, unsigned int b){
+    unsigned long long int result = 1;
+    // Result is equal to 1 because we are going to gradually multiply the result by a.  If we started with 0, everything would always remain 0.
+    for (unsigned int i = 0; i < b; i++){
         result = result * a;
     }
     return result;
 }
 
+// Separates a number into individual digits and stores them in a vector.
+vector<int> vectorize_digits(unsigned long long n){
+    vector<int> digits;
+    // Without this special case, vectorize_digits(0) would return an empty vector because the while loop would never run.
+    if (n == 0){
+        digits.push_back(0);
+        return digits;
+    }
+
+    while (n > 0){
+        // % 10 gives us the last digit, and dividing by 10 removes the last digit.
+        digits.push_back(n % 10);
+        n = n / 10;
+    }
+    // The digits are collected from right to left, so we reverse them to get their original order.
+    reverse(digits.begin(), digits.end());
+    return digits;
+}
+
+// Adds all the numbers that are in a vector.
+int sum_vector(vector<int> v){
+    int sum = 0;
+    for (int digit : v){
+        sum = sum + digit;
+    }
+    return sum;
+}
+
+// Converts a vector of digits into the string format.
+string vec_to_string(vector<int> vec){
+    string result = "[";
+    for (int i = 0; i < vec.size(); i++){
+        result += to_string(vec[i]);
+        if (i < vec.size() - 1){
+            result += ", ";
+        }
+    }
+    result += "]";
+    return result;
+}
+
+
 int main(int argc, char* argv[]){
-    // We need two arguments: a and b.
     if (argc != 3){
         cout << "Error: Please enter two integers: a and b." << endl;
         cout << "Example: ./pds 2 15" << endl;
         return 1;
     }
+
     // Keeps the inputs as strings first so we can check them.
     string aString = argv[1];
     string bString = argv[2];
-    // Checks if a contains only numbers.
+
+    if (aString[0] == '-' || bString[0] == '-'){
+        cout << "Error: a and b must be non-negative integers!" << endl;
+        return 1;
+    }
+
     for (char c : aString){
         if (c < '0' || c > '9')
         {
@@ -41,35 +85,29 @@ int main(int argc, char* argv[]){
         }
     }
 
-    // Checks if b contains only numbers.
     for (char c : bString){
         if (c < '0' || c > '9'){
             cout << "Error: integers only! Letters and other characters are not allowed." << endl;
             return 1;
         }
     }
+
     // Now we can turn the strings into numbers.
-    unsigned short a = stoi(aString);
-    unsigned short b = stoi(bString);
-    // Finds a to the power of b.
-    long n = power(a, b);
-    // Stops if the result was too large.
-    if (n == -1){
+    unsigned int a = stoi(aString);
+    unsigned int b = stoi(bString);
+
+    // Check for overflow before calculating a^b.
+    if (a > 0 && b * log10(a) >= 64 * log10(2)){
+        cout << "Error: a^b is too large, would cause overflow." << endl;
         return 1;
     }
 
-    // Finds the sum of all the digits.
-    long temp = n;
-    long digitSum = 0;
+    unsigned long long int n = power(a, b);
 
-    while (temp > 0){
-        // Gets the last digit and add it to the sum.
-        digitSum = digitSum + (temp % 10);
-        // Removes the last digit.
-        temp = temp / 10;
-    }
+    // Finds all the digits and then calculates their sum.
+    vector<int> digits = vectorize_digits(n);
+    int digitSum = sum_vector(digits);
 
-    // Prints the answer.
     cout << a << "^" << b << " = " << n << endl;
     cout << "Sum Of Digits: " << digitSum << endl;
 
@@ -91,4 +129,11 @@ https://www.studyplan.dev/intro-to-programming/include-directive
 
 https://www.programiz.com/cpp-programming/type-modifiers
 
+https://www.w3schools.com/cpp/cpp_ref_vector.asp
+
+https://www.geeksforgeeks.org/cpp/difference-between-long-int-and-long-long-int-in-c-cpp/ 
+
+https://www.geeksforgeeks.org/cpp/convert-vector-of-chars-to-string-in-cpp/ 
+
+https://www.geeksforgeeks.org/cpp/vector-in-cpp-stl/ 
 */
